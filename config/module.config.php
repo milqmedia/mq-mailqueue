09f1726bf15bb3814c0ce06e1cb1f931ea0caf5c
Yo-han <github@w3f.nl>
2015-03-05 11:39:22 +0100

    default config

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,18 @@
 <?php
 
 return array(
-    
+    'aws' => array(
+         'key'    				=> '<aws-key>',
+         'secret' 				=> '<aws-region>',
+         'region' 				=> '<aws-region>'
+    ),
+    'mailqueue' => array(
+	    'adminEmail' 			=> '',
+	 	'senderName' 			=> '',
+	 	'senderEmail' 			=> '', 
+	 	'numberOfEmailsPerRun' 	=> 250,  
+	 	'database' => array(
+		 	'entity' 			=> 'Application\Entity\MailQueue',
+	 	),
+    ),
 );
